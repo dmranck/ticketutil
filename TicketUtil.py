fcdd481a08f605885a788b3850a54def0abccd9c
Danny Ranck <dranck@redhat.com>
2016-12-16 15:37:10 -0500

    UPDATE BugzillaTicket to accept auth as tuple
    Instead of having auth, username, and password parameters for Bugzilla,
    this code makes it more consistent with RedmineTicket, which accepts a
    tuple for auth of the form auth=(<username>, <password>).
    
    Make README more consistent across all four tools.

--- TicketUtil.py
+++ TicketUtil.py
@@ -1,6 +1,5 @@
 import logging
 import re
-import os
 
 import gssapi
 import requests
@@ -11,7 +10,7 @@ __author__ = 'dranck, rnester, kshirsal'
 # Disable warnings for requests because we aren't doing certificate verification
 requests.packages.urllib3.disable_warnings()
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     logging.basicConfig(level=logging.DEBUG)
@@ -127,14 +126,18 @@ class BugzillaTicket(Ticket):
     """
     A BZ Ticket object. Contains BZ-specific methods for working with tickets.
     """
-
-    def __init__(self, base_url, project_key, ticket_id=None, auth=None, user=None, password=None):
+    def __init__(self, base_url, project_key, ticket_id=None, auth=None):
         self.ticketing_tool = 'Bugzilla'
 
-        # Right now, hardcode auth as 'kerberos', which is the only supported auth for BZ.
-        self.auth = auth if auth else 'kerberos'
+        # Kerberos is default auth if the auth param is not specified.
+        # A tuple of the form (<username>, <password>) can also be passed in.
+        if isinstance(auth, tuple):
+            self.auth = auth
+            username, password = auth
+            self.credentials = {"login": username, "password": password}
+        else:
+            self.auth = 'kerberos'
 
-        self.credentials = {"login": user, "password": password}
         self.token = None
 
         # BZ URLs
@@ -188,9 +191,10 @@ class BugzillaTicket(Ticket):
         We're using a Session to persist cookies across all requests made from the Session instance.
         :return s: Requests Session.
         """
-        if self.auth != 'rest':
-            return super()
+        if self.auth == 'kerberos':
+            return super(BugzillaTicket, self).create_requests_session()
 
+        # If basic authentication is passed in, generate a token to be used in all requests.
         try:
             s = requests.Session()
             r = s.get(self.auth_url, params=self.credentials, verify=False)
@@ -198,11 +202,12 @@ class BugzillaTicket(Ticket):
             resp = r.json()
             self.token = resp['token']
             logging.debug("Create requests session: Status Code: {0}".format(r.status_code))
-            logging.info("Successfully authenticated to {0} with token: {1}".format(self.ticketing_tool, self.token))
+            logging.info("Successfully authenticated to {0}.".format(self.ticketing_tool))
             return s
 
         # We log an error if authentication was not successful, because rest of the HTTP requests will not succeed.
-        except requests.RequestException as e:
+        # If authentication wasn't successful, a token will not be in resp. Add KeyError as exception.
+        except (KeyError, requests.RequestException) as e:
             logging.error("Error authenticating to {0}. No valid credentials were provided.".format(self.auth_url))
             logging.error(e.args[0])
 
@@ -887,6 +892,7 @@ class RedmineTicket(Ticket):
         else:
             logging.error("No ticket ID associated with ticket object. Set ticket ID with set_ticket_id(ticket_id)")
 
+
 def get_kerberos_principal():
     """
     Use gssapi to get the current kerberos principal.
@@ -897,6 +903,7 @@ def get_kerberos_principal():
         return str(gssapi.Credentials(usage='initiate').name).lower()
     except gssapi.raw.misc.GSSError:
         return None
+
 
 def main():
     """
