e7634d44ecec410f43f14181e33c0d897f867211
Danny Ranck <dranck@redhat.com>
2017-01-19 13:26:45 -0500

    FIX requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,15 @@
 
 from setuptools import setup
 
-
-def _get_requirements(file_name):
-    requirements = []
-    for line in open(file_name).readlines():
-        requirements.append(line)
-    return requirements
-
 setup(
     name='ticketutil',
     packages=['ticketutil'],
-    version='1.0.2',
+    version='1.0.3',
     description='Python ticketing utility supporting JIRA, RT, Redmine, and Bugzilla',
     author='Danny Ranck',
     author_email='dmranck@gmail.com',
     url='https://github.com/dmranck/ticketutil',
-    download_url='https://github.com/dmranck/ticketutil/tarball/1.0.2',
+    download_url='https://github.com/dmranck/ticketutil/tarball/1.0.3',
     keywords=['jira', 'bugzilla', 'rt', 'redmine', 'ticket', 'rest'],
-    install_requires=_get_requirements('requirements.txt')
+    install_requires=['gssapi>=1.2.0', 'requests>=2.9.1', 'requests-kerberos>=0.8.0']
 )
