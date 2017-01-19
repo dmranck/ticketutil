913cc7de2f099103b815f32afbe3ce1bbac7918a
Danny Ranck <dranck@redhat.com>
2017-01-19 13:07:38 -0500

    UPDATE version to 1.0.2

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ def _get_requirements(file_name):
 setup(
     name='ticketutil',
     packages=['ticketutil'],
-    version='1.0.0',
+    version='1.0.2',
     description='Python ticketing utility supporting JIRA, RT, Redmine, and Bugzilla',
     author='Danny Ranck',
     author_email='dmranck@gmail.com',
     url='https://github.com/dmranck/ticketutil',
-    download_url='https://github.com/dmranck/ticketutil/tarball/1.0.0',
+    download_url='https://github.com/dmranck/ticketutil/tarball/1.0.2',
     keywords=['jira', 'bugzilla', 'rt', 'redmine', 'ticket', 'rest'],
     install_requires=_get_requirements('requirements.txt')
 )
