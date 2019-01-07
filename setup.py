7ec2b21b906ef633b8cfd34120da392b0f9b89c1
Danny Ranck <dranck@redhat.com>
2019-01-07 16:53:15 -0500

    RELEASE ticketutil 1.4.2

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 """Setup for ticketutil."""
 
+from os import path
 from setuptools import setup
 
-with open('README.rst') as file:
-    long_description = file.read()
-with open('HISTORY.rst') as file:
-    long_description += '\n{0}'.format(file.read())
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst')) as f:
+    long_description = f.read()
+with open(path.join(this_directory, 'HISTORY.rst')) as f:
+    long_description += '\n{0}'.format(f.read())
 
 setup(
     name='ticketutil',
     packages=['ticketutil'],
-    version='1.4.1',
+    version='1.4.2',
     description='Python ticketing utility supporting JIRA, RT, Redmine, Bugzilla, and ServiceNow',
     long_description=long_description,
     author='Danny Ranck',
     author_email='dmranck@gmail.com',
     url='https://github.com/dmranck/ticketutil',
-    download_url='https://github.com/dmranck/ticketutil/tarball/1.4.1',
+    download_url='https://github.com/dmranck/ticketutil/tarball/1.4.2',
     keywords=['jira', 'bugzilla', 'rt', 'redmine', 'servicenow', 'ticket', 'rest'],
     install_requires=['gssapi>=1.2.0', 'requests>=2.6.0', 'requests-kerberos>=0.8.0']
 )
