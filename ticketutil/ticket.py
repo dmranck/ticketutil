b52f496f2fbd4d4b4a10ced47784738c3fc6b598
Andrew Thomas <anthomas@redhat.com>
2019-04-22 14:39:08 -0400

    ADD sso saml support

--- ticketutil/ticket.py
+++ ticketutil/ticket.py
@@ -107,6 +107,7 @@ class Ticket(object):
             s.verify = False
         if isinstance(self.auth, tuple):
             s.auth = self.auth
+            s.verify = True
 
         # Try to authenticate to auth_url.
         try:
